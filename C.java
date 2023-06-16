e1aa1eb74997df351fd7c763dfc803e3f9832752
Sravanthi <sravanthibgs@gmail.com>
2023-06-16 19:41:03 -0400

    change in C.java method 321 to 22

--- C.java
+++ C.java
@@ -5,7 +5,7 @@ class C {
  method21(){
    System.out.println("in method 21 ");
 }
-method 321(){
-  System.out.println("in method 321 ");
+method 22(){
+  System.out.println("in method 22 ");
 }
 }
